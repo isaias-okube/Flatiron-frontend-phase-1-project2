f49a6a57af18a3ca48b43e784c115930557e9d5a
ISAIAS <issuyep@yahoo.com>
2023-08-10 02:37:58 -0600

    add second function btn to main.js

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 //https://www.amiiboapi.com/api/amiibo/?name=mario
 // Elements
 const gameSelect = document.querySelector('#games');
+const amiiboSelect = document.querySelector('#amiibos');
 
 // Function calls
 getGames();
+getAmiibos();
 
 // Functions
 function getGames() {
@@ -13,11 +15,28 @@ function getGames() {
         .catch();
 }
 
+function getAmiibos() {
+    fetch('https://www.amiiboapi.com/api/amiibo/?name=mario')
+        .then(res => res.json())
+        .then(amiibos => renderAmiibos(amiibos.amiibo))
+        .catch();
+}
+
 function renderGameOptions(games) {
     games.forEach(game => {
         const option = document.createElement('option');
         option.value = game.name;
         option.textContent = game.name;
         gameSelect.appendChild(option);
+    });
+}
+
+function renderAmiibos(amiibos) {
+    console.log(amiibos);
+    amiibos.forEach(amiibo => {
+        const option = document.createElement('option');
+        option.value = amiibo.name;
+        option.textContent = amiibo.name;
+        amiiboSelect.appendChild(option);
     });
 }
