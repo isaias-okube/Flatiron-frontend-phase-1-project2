044c9bb897b1f1daaf7ad8c16241dd47a0152c5d
ISAIAS <issuyep@yahoo.com>
2023-08-10 02:54:08 -0600

    add 2 event handler functions

--- main.js
+++ main.js
@@ -7,19 +7,23 @@ const amiiboSelect = document.querySelector('#amiibos');
 getGames();
 getAmiibos();
 
-// Functions
+// Event listeners
+gameSelect.addEventListener('change', getAmiibosByGame);
+amiiboSelect.addEventListener('change', getAmiiboByName);
+
+//  Drop down Functions
 function getGames() {
     fetch('https://www.amiiboapi.com/api/amiiboseries/')
         .then(res => res.json())
         .then(games => renderGameOptions(games.amiibo))
-        .catch();
+        .catch(error => alert(error));
 }
 
 function getAmiibos() {
     fetch('https://www.amiiboapi.com/api/amiibo/?name=mario')
         .then(res => res.json())
-        .then(amiibos => renderAmiibos(amiibos.amiibo))
-        .catch();
+        .then(amiibos => renderAmiibosOptions(amiibos.amiibo))
+        .catch(error => alert(error));
 }
 
 function renderGameOptions(games) {
@@ -31,12 +35,28 @@ function renderGameOptions(games) {
     });
 }
 
-function renderAmiibos(amiibos) {
-    console.log(amiibos);
+function renderAmiibosOptions(amiibos) {
     amiibos.forEach(amiibo => {
         const option = document.createElement('option');
         option.value = amiibo.name;
         option.textContent = amiibo.name;
         amiiboSelect.appendChild(option);
     });
+}
+
+// Event handler functions
+function getAmiibosByGame(event) {
+    const gameName = event.target.value;
+    fetch(`https://www.amiiboapi.com/api/amiibo/?amiiboSeries=${gameName}`)
+        .then(res => res.json())
+        .then(amiibos => renderAmiibos(amiibos.amiibo))
+        .catch(error => alert(error));
+}
+
+function getAmiiboByName(event) {
+    const amiiboName = event.target.value;
+    fetch(`https://www.amiiboapi.com/api/amiibo/?name=${amiiboName}`)
+        .then(res => res.json())
+        .then(amiibos => renderAmiibos(amiibos.amiibo))
+        .catch(error => alert(error));
 }
