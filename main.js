c61dbb8d8bd504fcbbde96c71d22cdf6288c56e0
ISAIAS <issuyep@yahoo.com>
2023-08-10 02:25:51 -0600

    add function to main.js

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("connected")
+//https://www.amiiboapi.com/api/amiibo/?name=mario
+// Elements
+const gameSelect = document.querySelector('#games');
+
+// Function calls
+getGames();
+
+// Functions
+function getGames() {
+    fetch('https://www.amiiboapi.com/api/amiiboseries/')
+        .then(res => res.json())
+        .then(games => renderGameOptions(games.amiibo))
+        .catch();
+}
+
+function renderGameOptions(games) {
+    console.log(games);
+    games.forEach(game => {
+        const option = document.createElement('option');
+        option.value = game.name;
+        option.textContent = game.name;
+        gameSelect.appendChild(option);
+    });
+}
